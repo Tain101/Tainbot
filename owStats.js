3c89ab2d3ba50096af5b094dee782653b6dc56f6
Taylor Premo <Taylor Premo>
2016-08-10 20:12:54 -0400

    wrote groundwork for reminder command

--- owStats.js
+++ owStats.js
@@ -2,32 +2,33 @@
 
 module.exports = function() {
     // "use strict";
-    var fs      = require('fs'); //filesystem
-    var request = require('request');
+    var fs       = require('fs'); //filesystem
+    var request  = require('request');
+    let STAT_FILE = "owStatsJson.json";
 
     this.owStats = function(message, args) {
         if (args[0] === "set") {
             if (!args[1]) {
-                mybot.sendMessages("you need to give me your bnet! try again.");
+                mybot.sendMessage("you need to give me your bnet! try again.");
                 return false;
             }else{
                 setOwStats(message.author, args[1]);
-                mybot.sendMessages("your bnet has been saved!");
+                mybot.sendMessage("your bnet has been saved!");
                 return true;
             }
 
         } else {
             var owStatsJson = null;
-            var data = fs.readFileSync("owStatsJson.json", "utf8");
+            var data = fs.readFileSync(STAT_FILE, "utf8");
             owStatsJson = JSON.parse(data);
 
 
             var user = args[0] || message.author;
             if (owStatsJson.hasOwnProperty(user)) { //check if user already has linked their bnet
-                mybot.sendMessages("https://www.overbuff.com/players/pc/" + owStatsJson[user]);
+                mybot.sendMessage("https://www.overbuff.com/players/pc/" + owStatsJson[user]);
                 return true;
             } else {
-                mybot.sendMessages("you havent set up your bnet! try !owStat set [bnet]");
+                mybot.sendMessage("you havent set up your bnet! try !owStat set [bnet]");
                 return false;
             }
         }
@@ -35,21 +36,21 @@ module.exports = function() {
 
     function setOwStats(author, bnet) {
         var owStatsJson = null;
-        var data = fs.readFileSync("owStatsJson.json", "utf8");
+        var data = fs.readFileSync(STAT_FILE, "utf8");
         owStatsJson = JSON.parse(data);
         console.log(owStatsJson);
 
         bnet = bnet.replace("#", "-");
         owStatsJson[author] = bnet;
 
-        fs.writeFileSync("owStatsJson.json", JSON.stringify(owStatsJson), "utf8");
+        fs.writeFileSync(STAT_FILE, JSON.stringify(owStatsJson), "utf8");
 
     }
 
     this.updateOwStats = function (){
         console.log("time to update stats!");
         var owStatsJson = null;
-        var data = fs.readFileSync("owStatsJson.json", "utf8");
+        var data = fs.readFileSync(STAT_FILE, "utf8");
         owStatsJson = JSON.parse(data);
 
         for (var user in owStatsJson){
---
+++ reminders.js
@@ -0,0 +1,61 @@
+let MIN_TIMEOUT_DUR = 93600000;//26*60*60*1000 hours * minutes * millliseconds
+let REMINDER_FILE   = reminders.json;
+
+var fs     = require('fs');
+var chrono = require('chrono-node');
+
+this.createReminder = function (bot, message, args) {
+    var remindTime    = chrono.parse(args);
+    var remindMessage = getMessage(args);
+    var remindChannel = message;
+    /*TODO:
+    var remindVerify = if(message.contains("-verify"));
+    */
+    var reminder = new Reminder(remindTime, remindMessage, remindChannel);
+    reminder.save();
+    reminder.checkSetInterval();
+}
+let Reminder = function (time, message, channel, verify = false) {
+    let time    = time;
+    let message = message;
+    let channel = channel;
+    let verify  = verify;
+    let timeout = null;
+
+    this.checkSetTimeout = function () {
+        let now = new Date.getTime();
+        if(time - now < MIN_TIMEOUT_DUR && !timeout){
+            timeout = setTimeout(this.trigger, time-now);
+        }
+    };
+
+    this.trigger = function () {
+        bot.sendMessage(message, channel);
+        deleteReminder(this);
+    };
+
+    this.save = function () {
+        deleteReminder(this);//delete all previous entries for this reminder so user can update.
+        reminderList.push(this);
+        fs.writeFileSync(REMINDER_FILE, JSON.stringify(reminderList), "utf8");
+    }
+}
+
+this.loadReminders = function () {
+    let data = fs.readFileSync(REMINDER_FILE, "utf8");
+    let reminderListJSON = JSON.parse(data);
+    reminderList = [];
+    for(var reminder in reminderListJSON){
+        reminderList.push(reminder);
+    }
+
+}
+this.deleteReminder = function (reminder) {
+    let index = reminderList.findIndex(function (element) {
+        return (reminder.time       === element.time &&
+                reminder.message    === element.message &&
+                reminder.channel    === element.channel);
+    });
+    reminderList.splice(index, 1);
+    fs.writeFileSync(REMINDER_FILE, JSON.stringify(reminderList), "utf8");
+}
